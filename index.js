aff594a78537099144b4382e5245a75a1bafda18
Alex <ha.alex@outlook.com>
2021-03-10 18:34:54 -0500

    Solving for empty/nonalphanumeric cmds

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const isDev = process.env.NODE_ENV === 'development'
 const token = isDev ? process.env.TOKEN_DEV : process.env.TOKEN
 const prefix = isDev ? "$" : "!"
 
-const { Console } = require('console');
 const Discord = require('discord.js');
 const fs = require('fs');
 
@@ -29,10 +28,20 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+    const capAlert = isJustinCapping(msg);
+    if(capAlert) {
+        msg.react('🇸');
+        msg.react('💲');
+        return;
+    }
     if(!msg.content.startsWith(prefix) || msg.author.bot) return;
 
     const args = msg.content.slice(prefix.length).trim().split(/ +/);
     const cmd = args.shift().toLowerCase();
+
+    const alphanumeric = '^[a-z0-9]+$';
+
+    if(!cmd.match(alphanumeric)) return;
 
     try {
         client.commands.get(cmd).execute(msg, args, client);
@@ -40,4 +49,17 @@ client.on('message', msg => {
         console.error(error);
         msg.reply('There was an error trying to execute that command!');
     }
+    
+
 });
+
+function isJustinCapping(msg){
+    const justin = "217024547378036736";
+    const cap = "good morning everyone";
+
+    if(msg.author.id == justin && msg.content.toLowerCase().includes(cap)) {
+        return true;
+    } else {
+        return false;
+    }
+}
