a9a76384a9181f58b2abbbecfe376e94f5d753c1
Alex <ha.alex@outlook.com>
2021-09-22 21:11:51 -0400

    Enhance (#4)
    * Enhance command
    
    * Removed image
    
    * Fixed arg processing and added functionality for gifs

--- commands/enhance.js
+++ commands/enhance.js
@@ -1,35 +1,59 @@
 const request = require('request');
 const fs = require('fs');
 const sharp = require('sharp');
+const gifResize = require('@gumlet/gif-resize');
 
 const baseUrl = 'https://cdn.discordapp.com/emojis';
-const imgPath = './images/resized.png'
+const imgPath = './images/resized'
 
 function process(message, args, client) {
+    const filetype = args[0].startsWith("<a:") ? 'gif' : 'png'
     if(typeof(args[1]) === 'undefined') {
         message.reply("Please try again and provide a dimension")
     } else if (!isAllowedDimension(args[1])) {
         message.reply("Please provide a valid dimension")
     } else {
-        let emoteId = args[0].replace(/\D/g, "")
-        let url = `${baseUrl}/${emoteId}.png`
+        let emoteId = args[0].split(":")[2].replace(/\D/g, "")
+        let url = `${baseUrl}/${emoteId}.${filetype}`
         let dimension = parseInt(args[1], 10);
+
         request({url, encoding: null}, (err, response, buffer) => {
-            sharp(buffer).resize(dimension, dimension).toFormat('png').toBuffer().then(buffer => {
-                let resized = Buffer.from(buffer, 'base64')
-    
-                fs.writeFile(imgPath, resized, function() {
-                    console.log("Done")
-                })
-                
-                options = {}
-                options['files'] = [imgPath]
-    
-                message.reply(options)
-            })
+            switch(filetype) {
+                case 'gif':
+                    gifResize({
+                        width: dimension
+                    })(buffer).then(data => {
+                        let resized = Buffer.from(data,'base64')
+
+                        fs.writeFile(`${imgPath}.${filetype}`, resized, function() {
+                            console.log("Done")
+                        })
+                        
+                        options = {}
+                        options['files'] = [`${imgPath}.${filetype}`]
+            
+                        message.reply(options)
+                    })
+                    break;
+                case 'png':
+                    sharp(buffer).resize(dimension, dimension).toFormat(filetype).toBuffer().then(buffer => {
+                        let resized = Buffer.from(buffer, 'base64')
+            
+                        fs.writeFile(`${imgPath}.${filetype}`, resized, function() {
+                            console.log("Done")
+                        })
+                        
+                        options = {}
+                        options['files'] = [`${imgPath}.${filetype}`]
+            
+                        message.reply(options)
+                    })
+                    break;
+                default:
+                    console.log("No filetype")
+            }
         })
     }
-
 }
 
 function isAllowedDimension(dimension) {
